717932fc4e2c38c36df5eb59c0542f901421a8dd
Ayşu <aysuuaydiinn@gmail.com>
2020-10-16 17:48:15 +0300

    roll a ball int

---
+++ Assets/CoinScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(eulers: new Vector3(x: 15f, y: 2f, z: 92f) * Time.deltaTime);
+    }
+}
---
+++ Assets/PlayerBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviourScript : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public float forceMultiplier;
+    private float _moveHorizontal;
+    private float _moveVertical;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        _moveHorizontal = Input.GetAxis("Horizontal") * forceMultiplier;
+        _moveVertical = Input.GetAxis("Vertical") * forceMultiplier;
+        _rigidbody.AddForce(x: _moveHorizontal, y: 0, z: _moveVertical, ForceMode.Acceleration);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Cube"))
+            GameObject.Destroy(other.gameObject);
+    }
+}
